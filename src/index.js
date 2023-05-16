9e219b28635ac9cce105a1082a8438d59633393d
L-ItsLocked <L-itslocked@users.noreply.github.com>
2023-05-16 00:28:08 -0400

    Create index.js & add content from html

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  document.querySelector("#temp").innerHTML = `${temperature}`;
+  //   let icon = icon.setAttribute(
+  //     "src",
+  //     `https://api.openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  //   );
+  //   document.querySelector("#icon").innerHTML = `${icon}`;
+}
+
+function getTemperature(coordinates) {
+  let key = "acf4f9701d8363a6ed361252f2b74e65";
+  let city = "Reykjavik";
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+
+  axios.get(api).then(displayTemperature);
+}
+getTemperature();
